d118533bb37cf88266f848b012bd33cc844dece6
Muhammad Jahanzaib <jahanzaibm29@gmail.com>
2023-08-27 07:20:02 +0500

    bottom tab navigation

--- App.jsx
+++ App.jsx
@@ -1,83 +1,54 @@
-import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Button, Modal, Text} from 'react-native';
+import React from 'react';
+import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-function HomeScreen(props) {
-  console.warn(props.route.params);
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Text>Email : {props.route.params.email}</Text>
-    </View>
-  );
-}
-function Login(props) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Enter secret Key</Text>
-      <Button
-        title="Login"
-        onPress={() =>
-          props.navigation.navigate('Home', {email: 'jahanzaibm29@gmail.com'})
-        }
-      />
-    </View>
-  );
-}
-
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{
-            headerTitle: ()=><Button title='hola'/>,
-            headerRight:()=> <Button title='lola'/>,
-            headerStyle: {
-              backgroundColor: 'gold',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontSize: 30,
-              fontWeight: 'bold',
-            },
-          }}
-        />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'list' : 'list-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={Setting} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonWrapper: {
-    flex: 1,
-    justifyContent: 'flex-end',
-  },
-  modalContainer: {flex: 1, justifyContent: 'center'},
-  modalView: {
-    backgroundColor: '#fff',
-    paddingHorizontal: 20,
-    paddingVertical: 20,
-    margin: 10,
-    borderRadius: 10,
-    shadowColor: '#000',
-    elevation: 7,
-  },
-  modalText: {
-    fontSize: 25,
-    marginBottom: 10,
-  },
-});
-
+const HomeScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Ionicons name="home"/>
+      <Button title="hello" onPress={() => console.warn('hello')} />
+    </View>
+  );
+};
+const Setting = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Enter secret Key</Text>
+    </View>
+  );
+};
 export default App;

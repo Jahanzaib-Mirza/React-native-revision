e9a7ed704e02971e78c83681120154c71e5383d1
Muhammad Jahanzaib <jahanzaibm29@gmail.com>
2023-08-27 23:39:08 +0500

    Top Tab Navigation

--- App.jsx
+++ App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 const App = () => {
   return (
@@ -13,20 +13,19 @@ const App = () => {
         screenOptions={({route}) => ({
           tabBarIcon: ({focused, color, size}) => {
             let iconName;
-
+            size=20;
             if (route.name === 'Home') {
-              iconName = focused
-                ? 'home'
-                : 'home-outline';
+              iconName = focused ? 'home' : 'home-outline';
             } else if (route.name === 'Settings') {
               iconName = focused ? 'list' : 'list-outline';
             }
-
+            console.warn(typeof(size));
             // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
           tabBarActiveTintColor: 'tomato',
           tabBarInactiveTintColor: 'gray',
+          tabBarShowLabel: false,
         })}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={Setting} />
@@ -39,7 +38,7 @@ const HomeScreen = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
-      <Ionicons name="home"/>
+      <Ionicons name="home" />
       <Button title="hello" onPress={() => console.warn('hello')} />
     </View>
   );
